c2f64d40c6c0fb2f852f451aee3395690790e778
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2019-03-26 21:13:34 +0100

    Adds beginning of task 5
    Successfully simulates random walk in a potential, used some arbitrary parameters

--- 1D_rand_walk.py
+++ 1D_rand_walk.py
@@ -4,9 +4,10 @@ from scipy.stats import norm
 
 N = 1000
 steps = 100
-field = 2*steps
+field = 2*steps+1
 x_vec = np.linspace(-steps, steps, field)
 
+# This uses some weird matrix technic, cause I want to try to remove the inner for-loop
 particles = np.zeros([N, field])
 particles.T[steps] = np.ones(N)
 
@@ -26,4 +27,30 @@ ax2 = ax1.twinx()
 ax1.bar(x_vec, dist)
 ax2.plot(x_vec, fitted_pdf)
 
+plt.show()
+
+
+'''Random Walk in potential'''
+particles = np.zeros(N)
+k = 1
+h = 1
+beta = 1
+V = lambda x: k*x
+
+def P_min(x):
+    rel_prob = np.exp(-beta*(V(x-h)-V(x+h)))
+    Pp = 1/(1 + rel_prob)
+    Pm = 1 - Pp
+    return Pm
+
+
+for i in range(steps):
+    steps_vec = np.random.rand(N)
+    for x in particles:
+        steps_vec[steps_vec >= P_min(x)] = 1
+        steps_vec[steps_vec != 1] = -1
+    particles += steps_vec
+
+print(particles)
+plt.hist(np.sort(particles), x_vec)
 plt.show()
